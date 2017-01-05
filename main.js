1b30cd817a40b05abf2143aeba710dd42e23e5fd
Carlos Pereira <carlinhop@gmail.com>
2017-01-05 14:42:43 +0100

    Learning more features

--- main.js
+++ main.js
@@ -6,23 +6,24 @@ var dragged
 left.addEventListener("dragstart", 
 	function(event){
 		//event.preventDefault();
-		dragged = event.currentTarget.innerText;
+		event.dataTransfer.setData("text/plain",event.currentTarget.innerText);
+		//console.dir(event);
 	}, false);
 
 right.addEventListener("dragenter",
 	function(event){
 		event.preventDefault();
-		console.log("enter");
+		console.dir(event);;
 	}, false);
 
 right.addEventListener("dragover",
 	function(event){
 		event.preventDefault();
-		console.log(event);
+		//console.log(event);
 	}, false);
 
 right.addEventListener("drop",
 	function(event){
 		event.preventDefault();
-		right.innerText = dragged;
+		right.innerText = event.dataTransfer.getData("text/plain");
 	}, false);
